ccfff9d6f2398f9cfe294fb9a3eadc38cb6e321a
Muskan <chauhanmuskan37@gmail.com>
2025-12-18 20:58:25 +0530

    new updates.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API = axios.create({ baseURL: API_BASE_URL });
 
 export const fetchItems = () => API.get('/items');
 export const addItem = (item) => API.post('/items', item);
